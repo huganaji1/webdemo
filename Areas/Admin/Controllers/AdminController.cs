a554cc1c3a39243fdf67fb6f464e3dc049a708da
MayNha <ADMIN@DESKTOP-F4AOELP>
2024-04-21 22:22:43 +0700

    Add project files.

---
+++ Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webdemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("/homeadmin")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/FurnitureController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webdemo.Controllers
+{
+    [Route("fur")]
+    public class FurnitureController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
